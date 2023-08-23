e3d8404d6a8843424131b8fffcfb243dc50f5772
gaoyanqiang <gaoyanqiang@cambricon.com>
2023-08-23 17:05:47 +0800

    modify multi_draw.py

--- h100_350w_pcie/d.sh
+++ h100_350w_pcie/d.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 data_type=fp16_half
-python3 multi_draw.py " power.draw.instant [W]" " clocks.current.sm [MHz]" " utilization.gpu [%]" ${data_type}_random.csv ${data_type}_allzero.csv
+#python3 multi_draw.py " power.draw.instant [W]" " clocks.current.sm [MHz]" " utilization.gpu [%]" ${data_type}_random.csv ${data_type}_allzero.csv
+python3 multi_draw.py " power.draw.instant [W]" " clocks.current.sm [MHz]" " temperature.gpu" ${data_type}_random.csv ${data_type}_allzero.csv
--- h100_350w_pcie/multi_draw.py
+++ h100_350w_pcie/multi_draw.py
@@ -9,6 +9,17 @@ param_A = sys.argv[1]  # 参数A
 param_B = sys.argv[2]  # 参数B
 param_C = sys.argv[3]  # 参数C
 
+def convert_timestamp(timestamp):
+    # 将时间戳字符串拆分成日期和时间部分
+    date, time = timestamp.split(' ')
+    
+    # 将时间部分拆分成小时、分钟和秒部分
+    hh, mm, ss = map(float, time.split(':'))
+    
+    # 执行单位换算并返回结果
+    converted_ts = int((((hh * 60 + mm) * 60) + ss) * 1000)
+    return converted_ts
+
 # 创建大图
 fig, axs = plt.subplots(len(files), 1, figsize=(8, 6 * len(files)))
 
@@ -27,16 +38,25 @@ for i, file in enumerate(files):
         idx_A = headers.index(param_A)
         idx_B = headers.index(param_B)
         idx_C = headers.index(param_C)
+        idx_X = headers.index("timestamp")
 
         # 初始化x和y的列表
         x_vals = []
         y_vals_A = []
         y_vals_B = []
         y_vals_C = []
+        x_min_val = 0
 
         # 读取CSV文件中的数据
         for xi, row in enumerate(reader):
-            x_vals.append(xi)
+            x_val = float(convert_timestamp(row[idx_X]))
+            if (xi == 0) :
+                x_min_val = x_val
+                x_val = 0
+            else :
+                x_val = x_val - x_min_val
+            #x_vals.append(xi)
+            x_vals.append(x_val)
             # 从参数A和参数B对应的列中提取第一个浮点数
             y_vals_A.append(float(re.findall(r"\d+\.\d+|\d+", row[idx_A])[0]))
             y_vals_B.append(float(re.findall(r"\d+\.\d+|\d+", row[idx_B])[0]))
@@ -47,7 +67,7 @@ for i, file in enumerate(files):
         ax1 = axs[i]
 
         color = 'tab:red'
-        ax1.set_xlabel('timestamp(20ms)')
+        ax1.set_xlabel('timestamp(ms)')
         lineA, = ax1.plot(x_vals, y_vals_A, color=color, label=param_A)
         ax1.set_ylabel(param_A, color=color)
         ax1.tick_params(axis='y', labelcolor=color)
